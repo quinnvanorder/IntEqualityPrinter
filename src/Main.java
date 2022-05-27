9423e5df12167d740e846134d35e4418e6dcf89e
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-05-27 13:21:06 -0700

    Built framework and detailed challenge in main

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/*
+Write a method printEqual with 3 parameters of type int. The method returns void.
+If one of the parameters is less than 0 print "Invalid Value"
+If all the numbers are equal print "All numbers are Equal".
+If all the numbers are different print "All numbers are different"
+Otherwise print "Neither all are equal or different"
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
